bfb4ad26aa99287f26577c24090481983a3bb62b
João Gois <joao.gois@trustly.com>
2024-09-19 19:30:52 -0300

    Aula 2.1 Criando o primeiro programa Java

---
+++ 2.1 Criando o primeiro programa Java/OlaMergulhador.java
@@ -0,0 +1,12 @@
+// Declarando o nome de uma classe
+// Abrindo e fechando um bloco de código com {}
+public class OlaMergulhador {
+	
+	// Declarando um método que é o ponto de entrada para iniciar o programa
+	// main é o único nome possível desse método
+	public static void main(String[] args) {
+		// System é uma classe da API do Java
+		System.out.println("Olá, mergulhador!");
+	}
+
+}
