f154a441607fece19275120b3e85068439b67669
João Gois <joao.gois@trustly.com>
2025-01-03 16:51:51 -0300

    Aula Variáveis

---
+++ 2.7 Working with variables/AulaVariaveis.java
@@ -0,0 +1,10 @@
+public class AulaVariaveis {
+	public static void main(String[] args) {
+		// Declaração
+		int minhaIdade;
+		// Atribuição de valor
+		minhaIdade = 40;
+		
+		System.out.println(minhaIdade);
+	}
+}
