d8821e67a0ee15356738a92a309fe7786d4626af
João Gois <joao.gois@trustly.com>
2025-01-03 16:47:03 -0300

    Operadores Aritméticos

---
+++ 2.8 Operadores Aritméticos/OperadoresAritmeticos.java
@@ -0,0 +1,12 @@
+public class OperadoresAritmeticos {
+	
+	public static void main(String[] args) {
+		int minhaIdade = 40;
+		int suaIdade = 25;
+		
+		int totalIdades = minhaIdade + suaIdade;
+		
+		System.out.println("Total das idades: " + totalIdades);
+	}
+	
+}
